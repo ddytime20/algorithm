4e30a6afb44b338204eb682ddafca68bb1698728
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-07-27 16:09:48 +0800

    Update bm.h

--- lib/bm.h
+++ lib/bm.h
@@ -11,6 +11,16 @@ struct BmS
     unsigned char BmBc[ASIZE];
 };
 
+struct BmsS
+{
+    unsigned char BmBc[ASIZE];
+};
+
+void BMs_Prepare(unsigned char *pat; int patlen, struct BmsS *bms);
+
+void BMs_Search(unsigned char *text, int textlen, unsigned char *pat,
+        int patlen, struct BmsS *bms);
+
 void BM_Prepare(unsigned char *pat, int patlen, struct BmS *bms);
 
 void BM_Search(unsigned char *text, int textlen, unsigned char *pat,
