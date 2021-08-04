b7c2869575b34d5c8f455d349e9234e8e5e951d3
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-08-04 16:26:18 +0800

    Create bheap.h

---
+++ lib/bheap.h
@@ -0,0 +1,25 @@
+#ifndef _B_HEAP_H_
+
+#define _B_HEAP_H_
+
+#define MinData (-32767)
+typedef int tp
+
+typedef struct heap{
+    tp *array;
+    int size;
+    int max;
+}heap_t;
+
+heap_t *Heap_Create(int max);
+void Heap_Destroy(heap_t *h);
+int Heap_IsFull(heap_t *h);
+int Heap_IsEmpty(heap_t *h);
+void Heap_Empty(heap_t *h);
+void Heap_Insert(heap_t *h, tp x);
+tp Heap_Delete(heap *h);
+tp Heap_FindMin(heap *h);
+
+
+#endif
+
