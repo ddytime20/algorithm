3c222b1a91e6a80e35d7936a6ae7816440ce8141
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-08-05 17:15:40 +0800

    Update lheap.h

--- lib/lheap.h
+++ lib/lheap.h
@@ -4,19 +4,19 @@
 
 typedef int et;
 
-struct lheap_t {
+typedef struct lheap_t {
     int npl;
     et  element;
-    struct lheap_t left;
-    struct lheap_t right;
-};
+    struct lheap_t *left;
+    struct lheap_t *right;
+}lheap_t;
 
 typedef struct lheap_t lnode_t;
 
 #define Lheap_Insert(x, h) (h = Lheap_Insert1((x), h))
 #define Lheap_Delete(h) (h = Lheap_Delete1(h))
 
-lheap_t * Lheap_Create(void);
+lheap_t *Lheap_Create(void);
 lheap_t *Lheap_Insert1(et x, lheap_t *h);
 lheap_t *Lheap_Delete1(lheap_t *h);
 lheap_t *Lheap_Merge(lheap_t *h1, lheap_t *h2);
