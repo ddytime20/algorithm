0f225b292e28f3df487b39e9ced9b5165210ca53
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-08-07 09:57:50 +0800

    Create binqueue.h

---
+++ lib/binqueue.h
@@ -0,0 +1,32 @@
+#ifndef _BIN_QUEUE_H_
+
+#define _BIN_QUEUE_H_
+
+#define MAX_TREE (14) /* store 2^14 - 1 */
+#define CAPACITY (16383) /* 2^14 - 1 */ 
+typedef int ET;
+
+typedef struct BinNode
+{
+    ET Element;
+    struct BinNode LeftChild;
+    struct BinNode NextSibling;
+}BinNode;
+
+typedef struct BinQueue
+{
+    int Size;
+    struct BinNode *Tree[MAX_TREE];
+}BinQueue;
+
+BinQueue *Bqueue_Create(void);
+BinQueue *Bqueue_Merge(BinQueue *q1, BinQueue *q2);
+BinQueue *Bqueue_Insert(ET x, BinQueue *q);
+ET Bqueue_Delete(BinQueue *q);
+ET Bqueue_FindMin(BinQueue *q);
+void Bqueue_Destroy(BinQueue *q);
+int Bqueue_IsFull(BinQueue *q);
+int Bqueue_IsEmpty(BinQueue *q);
+
+
+#endif
