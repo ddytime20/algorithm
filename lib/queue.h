d33336429d9bba235b6fb7162c68235fb0d50556
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-08-12 13:56:05 +0800

    Create queue.h

---
+++ lib/queue.h
@@ -0,0 +1,27 @@
+#ifndef _QUEUE_H_
+
+#define _QUEUE_H_
+
+#define QUEUE_MINSIZE 5
+#define QUEUE_INVALID (~0UL)
+typedef unsigned long ElementType;
+
+typedef struct Queue
+{
+    int Capacity;
+    int Front;
+    int Rear;
+    int Size;
+    ElementType Array[1];
+}Queue;
+
+Queue *CreateQueue(int max);
+void Queue_Destroy(Queue *Q);
+int Queue_IsFull(Queue *Q);
+int Queue_IsEmpty(Queue *Q);
+void Queue_Enqueue(ElementType x, Queue *Q);
+ElementType Queue_Dequeue(Queue *Q);
+ElementType Queue_Front(Queue *Q);
+
+
+#endif
