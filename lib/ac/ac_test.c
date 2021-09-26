1c78e28147af64afd17491337a2b94544831a237
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2021-09-26 16:25:23 +0800

    Update ac_test.c

--- lib/ac/ac_test.c
+++ lib/ac/ac_test.c
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "basetype.h"
 #include "debug.h"
@@ -11,7 +12,7 @@
 
 typedef enum pid_type
 {
-    pid_type_test,
+    pid_type_test = 1,
     pid_type_number,
     pid_type_find,
     pid_type_main,
@@ -27,19 +28,31 @@ typedef struct pattern
 
 ACHANDLE Handle;
 pattern_s Patterns[] = {
-    {(Byte *)"test", 4, pid_type_test},
-    {(Byte *)"number", 6, pid_type_number},
-    {(Byte *)"find", 4, pid_type_find},
     {(Byte *)"main", 4, pid_type_main},
+    {(Byte *)"test", 4, pid_type_test},
     {(Byte *)"printf", 6, pid_type_printf},
 };
 
+Byte *String = (Byte *)"maintestestjprintfainabcdfghjkmainj";
+
+static void ac_test_result_print(Byte *pStart, Byte *pEnd)
+{
+    printf("hit pid ");
+
+    while(pStart <= pEnd)
+    {
+        printf("%c",*pStart);
+        pStart++;
+    } 
+    printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
     Uint ret;
     Uint Num;
     Uint uiLoop;
+    ac_full_result_s Result;
     Handle = Ac_CreateHandle();
     if (INVALID_HANDLE == Handle)
     {
@@ -57,10 +70,42 @@ int main(int argc, char *argv[])
         }
     }
 
+    if (ERROR_SUCCESS != ret)
+    {
+        printf("ac_test add pattern failed \n");
+    }
+
     ret = Ac_Compile(Handle); 
     if (ERROR_SUCCESS != ret)
     {
         printf("Ac_Compile failed \n");
+        return 0;
+    }
+    else
+    {
+        printf("ac compile success\n");
+    }
+    
+    memset(&Result, 0, sizeof(ac_full_result_s));
+    Ac_FullSearch(Handle, String, strlen(String), &Result);
+    if (Result.PidNum)
+    {
+        int pidnum  = Result.PidNum;
+        int index;
+        int loop;
+        ac_hit_pid_s *hitpid;
+        printf("result hit pid number %d \n", pidnum);
+        printf("*************************************\n");
+        for (index = 0; index < pidnum; index++)
+        {
+            hitpid = &Result.HitPid[index];
+            printf("hit pid ID %d \n",hitpid->PidID);
+            printf("hit count %d \n", hitpid->HitCount);
+            for(loop = 0; loop < hitpid->HitCount; loop++)
+            {
+                ac_test_result_print(hitpid->pHitStart[loop], hitpid->pHitEnd[loop]);
+            }
+        }
     }
 
     return 0;
