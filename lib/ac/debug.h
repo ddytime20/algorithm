34742a0108a3029b98f0251c614bb6f1959f348f
ddytime20 <52104496+ddytime20@users.noreply.github.com>
2022-01-13 14:37:23 +0800

    Update debug.h

--- lib/ac/debug.h
+++ lib/ac/debug.h
@@ -7,9 +7,25 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#ifndef _STDARG_H
+#include <stdarg.h>
+#endif
+
 #define PRINTF_ATTR(fmt, args) __attribute__((format(printf, fmt, args)))
 
-extern void AC_PRINTF(char *format, ...) PRINTF_ATTR(1, 2);
+#define DEBUG_STRING(X)     #X
+#define DEBUG_TOSTR(X)      DEBUG_STRING(X)
+
+#define DEBUG_LOCATION_FUNC __func__
+#define DEBUG_LOCATION_LINE DEBUG_TOSTR(__LINE__)
+
+void Ac_Printf(const char *, const char *, const char *, ...) PRINTF_ATTR(3,4);
+
+#define AC_PRINTF(format, ...)    \
+   do{  \
+      Ac_Printf(DEBUG_LOCATION_FUNC, DEBUG_LOCATION_LINE, (format), ## __VA_ARGS__);  \
+   }while(0) 
+
 
 
 #endif
